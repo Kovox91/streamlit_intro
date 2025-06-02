4bd3c21f78bfd8eb931040e53f5d1a9b67cc1334
Kovox91 <sascha.maschmann@hotmail.de>
2025-06-02 10:19:30 +0200

    Initialized App

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Worldwide Analysis of Quality of Life and Economic Factors")
+st.header("Worldwide Analysis of Quality of Life and Economic Factors")
+st.subheader("This app enables you to explore the relationships between poverty, life expectancy, and GDP across various countries and years. Use the panels to select options and interact with the data.")
